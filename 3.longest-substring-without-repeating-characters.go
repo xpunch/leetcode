2917a13217b02654d363cce3802effebe9f35b64
johnson <chengqiaosheng@gmail.com>
2019-05-01 22:47:45 +0800

    3.longest-substring-without-repeating-characters
    version 4

--- 3.longest-substring-without-repeating-characters.go
+++ 3.longest-substring-without-repeating-characters.go
@@ -6,24 +6,16 @@
 func lengthOfLongestSubstring(s string) int {
 	cache := make(map[byte]int)
 	var result, longest int
-	var i, j int
-	for j < len(s) {
-		if x, ok := cache[s[j]]; ok {
-			if i <= x {
-				longest = j - i
-				if longest > result {
-					result = longest
-				}
-				i = x + 1
-				continue
-			}
+	var i int
+	for j, c := range []byte(s {
+		if x, ok := cache[c]; ok && x >= i {
+			i = x + 1
 		}
-		cache[s[j]] = j
-		j++
-	}
-	longest = j - i
-	if longest > result {
-		result = longest
+		longest = j - i + 1
+		if longest > result {
+			result = longest
+		}
+		cache[c] = j
 	}
 	return result
 }
